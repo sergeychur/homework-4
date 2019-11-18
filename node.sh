a5b224a365b8ae95e433508e48ce05632e28c121
sergeychur <churicovser@mail.ru>
2019-11-18 23:07:29 +0300

    enlarged nodes number

--- node.sh
+++ node.sh
@@ -3,5 +3,5 @@ java -Dwebdriver.chrome.driver="./chromedriver" \
     -jar selenium-server-standalone-3.141.59.jar \
     -role node \
     -hub http://127.0.0.1:4444/grid/register \
-    -browser browserName=chrome,maxInstances=2 \
-    -browser browserName=firefox,maxInstances=2
+    -browser browserName=chrome,maxInstances=10 \
+    -browser browserName=firefox,maxInstances=10
